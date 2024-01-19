40a5063a5506ee7b8d558f11bf422d2f51583c69
empassaro <emanuele.passaro@emeal.nttdata.com>
2024-01-19 10:12:14 +0100

    PN-9330 fix: removed required fields.

--- src/test/java/F24Generator.java
+++ src/test/java/F24Generator.java
@@ -15,9 +15,9 @@ import java.text.ParseException;
 
 public class F24Generator {
 
-    public static void main(String[] args) throws IOException, ResourceException, ProcessingException, ParseException {
+    public static void main(String[] args) throws IOException, ResourceException, ProcessingException {
         //String simplifiedJson = "src/test/resources/input/f24simplified.json";
-        String simplifiedJson = "F24-PDF\\src\\test\\resources\\input\\f24simplified.json";
+        String simplifiedJson = "src\\test\\resources\\input\\f24simplified.json";
         String simplifiedString = new String(Files.readAllBytes(Paths.get(simplifiedJson)));
 
         F24Simplified f24Simplified = new ObjectMapper().readValue(simplifiedString, F24Simplified.class);
@@ -25,10 +25,10 @@ public class F24Generator {
         simplifiedValidator.validate();
 
         //Files.write(Path.of("src/test/resources/output/simplified.pdf"), PDFCreatorFactory.createPDFCreator(f24Simplified).createPDF());
-        Files.write(Path.of("F24-PDF\\src\\test\\resources\\output\\f24simplified.pdf"), PDFCreatorFactory.createPDFCreator(f24Simplified).createPDF());
+        Files.write(Path.of("src\\test\\resources\\output\\f24simplified.pdf"), PDFCreatorFactory.createPDFCreator(f24Simplified).createPDF());
 
         //String standardJson = "src/test/resources/input/f24standard.json";
-        String standardJson = "F24-PDF\\src\\test\\resources\\input\\f24standard.json";
+        String standardJson = "src\\test\\resources\\input\\f24standard.json";
         String standardString = new String(Files.readAllBytes(Paths.get(standardJson)));
 
         F24Standard f24Standard = new ObjectMapper().readValue(standardString, F24Standard.class);
@@ -36,10 +36,10 @@ public class F24Generator {
         standardValidator.validate();
 
         //Files.write(Path.of("src/test/resources/output/standard.pdf"), PDFCreatorFactory.createPDFCreator(f24Standard).createPDF());
-        Files.write(Path.of("F24-PDF\\src\\test\\resources\\output\\f24standard.pdf"), PDFCreatorFactory.createPDFCreator(f24Standard).createPDF());
+        Files.write(Path.of("src\\test\\resources\\output\\f24standard.pdf"), PDFCreatorFactory.createPDFCreator(f24Standard).createPDF());
 
         //String exciseJson = "src/test/resources/input/f24excise.json";
-        String exciseJson = "F24-PDF\\src\\test\\resources\\input\\f24excise.json";
+        String exciseJson = "src\\test\\resources\\input\\f24excise.json";
         String exciseString = new String(Files.readAllBytes(Paths.get(exciseJson)));
 
         F24Excise f24Excise = new ObjectMapper().readValue(exciseString, F24Excise.class);
@@ -47,10 +47,10 @@ public class F24Generator {
         exciseValidator.validate();
 
         //Files.write(Path.of("src/test/resources/output/excise.pdf"), PDFCreatorFactory.createPDFCreator(f24Excise).createPDF());
-        Files.write(Path.of("F24-PDF\\src\\test\\resources\\output\\f24excise.pdf"), PDFCreatorFactory.createPDFCreator(f24Excise).createPDF());
+        Files.write(Path.of("src\\test\\resources\\output\\f24excise.pdf"), PDFCreatorFactory.createPDFCreator(f24Excise).createPDF());
 
         //String elidJson = "src/test/resources/input/f24elide.json";
-        String elidJson = "F24-PDF\\src\\test\\resources\\input\\f24elide.json";
+        String elidJson = "src\\test\\resources\\input\\f24elide.json";
         String elidString = new String(Files.readAllBytes(Paths.get(elidJson)));
 
         F24Elid f24Elid = new ObjectMapper().readValue(elidString, F24Elid.class);
@@ -58,7 +58,7 @@ public class F24Generator {
         elidValidator.validate();
 
         //Files.write(Path.of("src/test/resources/output/elid.pdf"), PDFCreatorFactory.createPDFCreator(f24Elid).createPDF());
-        Files.write(Path.of("F24-PDF\\src\\test\\resources\\output\\f24elid.pdf"), PDFCreatorFactory.createPDFCreator(f24Elid).createPDF());
+        Files.write(Path.of("src\\test\\resources\\output\\f24elid.pdf"), PDFCreatorFactory.createPDFCreator(f24Elid).createPDF());
 
         System.out.println("pages elid: "+ PDFCreatorFactory.createPDFCreator(f24Elid).getPagesAmount());
         System.out.println("pages excise: "+ PDFCreatorFactory.createPDFCreator(f24Excise).getPagesAmount());
